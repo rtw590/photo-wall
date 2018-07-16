dbf5ad59b149b1e0b4aeb560a4a90ec9db851718
Rob Waller <r.t.waller@gmail.com>
2018-07-16 13:52:48 -0500

    Vue displaying full feed from backend. TODO Change express singlePost route

--- server/controllers/Posts.js
+++ server/controllers/Posts.js
@@ -3,54 +3,14 @@ let Post = require("../models/post");
 
 module.exports = {
   index(req, res) {
-    console.log("backend hit");
-    res.send([
-      {
-        title: "My Friend",
-        src: "1.png",
-        _id: 1,
-        likes: 5,
-        comments: [
-          {
-            user: "Mike",
-            body: "This is mikes comment"
-          },
-          {
-            user: "Dan",
-            body: "This is dan comment"
-          },
-          {
-            user: "Smith",
-            body: "This is Smiths comment"
-          }
-        ]
-      },
-      {
-        title: "Catdog",
-        src: "ksg.png",
-        _id: 2
-      },
-      {
-        title: "Man o meen",
-        src: "lebron.png",
-        _id: 3
-      },
-      {
-        title: "My ",
-        src: "better-together.jpg",
-        _id: 4
-      },
-      {
-        title: "My Shoe",
-        src: "vertical.jpg",
-        _id: 5
-      },
-      {
-        title: "Yoyoyoy",
-        src: "1.png",
-        _id: 6
+    Post.find({}, function(err, posts) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(posts);
+        res.send(posts);
       }
-    ]);
+    });
   },
   singlePost(req, res) {
     res.send({
